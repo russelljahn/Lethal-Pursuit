1b8244fd3f66f87ab6657e812d9143496a330991
russelljahn <russelljahn@gmail.com>
2014-01-17 12:52:06 -0600

    Added tilt interpolation, so now movement tilting is smooth.

--- SpaceshipPrototype/Assets/Scripts/Spaceship.cs
+++ SpaceshipPrototype/Assets/Scripts/Spaceship.cs
@@ -8,53 +8,64 @@ public class Spaceship : MonoBehaviour {
 	public Vector3 acceleration = new Vector3(200.0f, 150.0f, 200.0f);
 	public Vector3 maxVelocity  = new Vector3(0.0f, 10000.0f, 10000.0f);
 
+	public float tiltSpeed = 2.0f;
+
 	private Rigidbody rigidbody;
 	private ParticleSystem flames;
 	public GameObject pivot;
 
-	private Vector3 rightTiltRotation = new Vector3( 0.0f,  0.0f,  30.0f);
-	private Vector3 leftTiltRotation  = new Vector3( 0.0f,  0.0f, -30.0f);
-	private Vector3 upTiltRotation    = new Vector3(-30.0f, 0.0f,   0.0f);
-	private Vector3 downTiltRotation  = new Vector3( 30.0f, 0.0f,   0.0f);
+	public Vector3 rightTiltRotation = new Vector3(  0.0f,  0.0f,  30.0f);
+	public Vector3 leftTiltRotation  = new Vector3(  0.0f,  0.0f, -30.0f);
+	public Vector3 upTiltRotation    = new Vector3(-30.0f,  0.0f,   0.0f);
+	public Vector3 downTiltRotation  = new Vector3( 30.0f,  0.0f,   0.0f);
 
-	private Vector3 downRightTiltRotation  = new Vector3(  30.0f, 0.0f,  30.0f);
-	private Vector3 downLeftTiltRotation   = new Vector3(  30.0f, 0.0f, -30.0f);
-	private Vector3 upRightTiltRotation    = new Vector3( -30.0f, 0.0f,  30.0f);
-	private Vector3 upLeftTiltRotation     = new Vector3( -30.0f, 0.0f, -30.0f);
+	public Vector3 downRightTiltRotation  = new Vector3(  30.0f,  0.0f,  30.0f);
+	public Vector3 downLeftTiltRotation   = new Vector3(  30.0f,  0.0f, -30.0f);
+	public Vector3 upRightTiltRotation    = new Vector3( -30.0f,  0.0f,  30.0f);
+	public Vector3 upLeftTiltRotation     = new Vector3( -30.0f,  0.0f, -30.0f);
 	
+
+
 
 	// Use this for initialization
 	void Start () {
 		rigidbody = GetComponent<Rigidbody>();
 		flames = transform.FindChild("Particle System").GetComponent<ParticleSystem>();
 	}
-	
+
+
+
 
 	void FixedUpdate () {
 	
+		HandleMovement();
+		HandleTilt();
+
+	}
+
+
+
+
+	void HandleMovement() {
 		float xTilt = Input.GetAxis("Horizontal");
 		float yTilt = Input.GetAxis("Vertical");
-
+		
 		float zForce;
-
+		
 		rigidbody.velocity = new Vector3(
 			Mathf.Min(rigidbody.velocity.x, maxVelocity.x),
 			Mathf.Min(rigidbody.velocity.y, maxVelocity.y),
 			Mathf.Min(rigidbody.velocity.z, maxVelocity.z)
-		);
-
-		if (Input.GetButton("Boost")) { //Spacebar by default will make it move forward
+			);
+		
+		if (Input.GetButton("Boost")) {
 			rigidbody.AddRelativeForce (
 				transform.InverseTransformDirection(Vector3.forward)*acceleration.z*Time.deltaTime
-			);
-//			flames.loop = true;
+				);
 		}
-		else {
-//			flames.loop = false;
-		}
-
+		
 		RaycastHit hit;
-
+		
 		float horizontalDistanceToRaycast = 200.0f;
 		/* Left/Right movement if not going to collide... */
 		if (!Physics.Raycast(pivot.transform.position, Vector3.right*xTilt, out hit, horizontalDistanceToRaycast)) {
@@ -64,7 +75,7 @@ public class Spaceship : MonoBehaviour {
 				)
 			);
 		}
-
+		
 		float verticalDistanceToRaycast = 70.0f;
 		/* Up/Down movement if not going to collide... */
 		if (!Physics.Raycast(pivot.transform.position, Vector3.up*yTilt, out hit, verticalDistanceToRaycast)) {
@@ -74,60 +85,57 @@ public class Spaceship : MonoBehaviour {
 				)
 			);
 		}
+	}
 
 
+
+
+	void HandleTilt() {
+
+		float xTilt = Input.GetAxis("Horizontal");
+		float yTilt = Input.GetAxis("Vertical");
+
+		Vector3 targetRotationEuler = Vector3.zero;
+		
 		if (xTilt == 0) {
 			if (yTilt == 0) {
-				this.transform.localRotation = Quaternion.identity;
+				;
 			}
 			else if (yTilt < 0) {
-				this.transform.localRotation = Quaternion.Euler(downTiltRotation);
+				targetRotationEuler = downTiltRotation;
 			}
 			else if (yTilt > 0) {
-				this.transform.localRotation = Quaternion.Euler(upTiltRotation);
+				targetRotationEuler = upTiltRotation;
 			}
 		}
 		else if (xTilt < 0) {
 			if (yTilt == 0) {
-				this.transform.localRotation = Quaternion.Euler(leftTiltRotation);
+				targetRotationEuler = leftTiltRotation;
 			}
 			else if (yTilt < 0) {
-				this.transform.localRotation = Quaternion.Euler(downLeftTiltRotation);
+				targetRotationEuler = downLeftTiltRotation;
 			}
 			else if (yTilt > 0) {
-				this.transform.localRotation = Quaternion.Euler(upLeftTiltRotation);
+				targetRotationEuler = upLeftTiltRotation;
 			}
 		}
 		else if (xTilt > 0) {
 			if (yTilt == 0) {
-				this.transform.localRotation = Quaternion.Euler(rightTiltRotation);
+				targetRotationEuler = rightTiltRotation;
 			}
 			else if (yTilt < 0) {
-				this.transform.localRotation = Quaternion.Euler(downRightTiltRotation);
+				targetRotationEuler = downRightTiltRotation;
 			}
 			else if (yTilt > 0) {
-				this.transform.localRotation = Quaternion.Euler(upRightTiltRotation);
+				targetRotationEuler = upRightTiltRotation;
 			}
 		}
-
-
-//		Vector3 rotationAngle = transform.localRotation.eulerAngles;
-//
-//		float zAngle = transform.localRotation.eulerAngles.z;
-//
-//		Debug.Log ("zAngle: " + zAngle);
-//
-//		if ((zAngle >= 345.0f && zAngle <= 360.0f) || (zAngle <= 15.0f && zAngle >= -15.0f)) {
-//			Debug.Log ("Able to tilt ship up/down!");
-//			transform.RotateAround(
-//				pivot.transform.position, 
-//				Vector3.right, 
-//				-Time.deltaTime*yTilt*acceleration.x*0.01f
-//			);
-//
-//		}
-	
 		
-
+		transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(targetRotationEuler), tiltSpeed*Time.deltaTime);
 	}
+
+	
+
+
+
 }
