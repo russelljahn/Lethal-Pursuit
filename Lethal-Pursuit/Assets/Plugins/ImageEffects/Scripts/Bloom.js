7d77adedca16cb226caa0ad70a310840f359d820
russelljahn <russelljahn@gmail.com>
2014-02-03 17:40:16 -0600

    Adding a bunch of 3rd party libraries, such as NGUI and Gem Shaders.

---
+++ Lethal-Pursuit/Assets/Plugins/ImageEffects/Scripts/Bloom.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+@script RequireComponent (IndieEffects)
+@script AddComponentMenu ("Indie Effects/Bloom")
+import IndieEffects;
+
+private var bloomMat : Material;
+var bloomShader : Shader;
+@range(0,2)
+var amount : float;
+
+function Start () {
+	bloomMat = new Material(bloomShader);
+}
+
+function Update () {
+	bloomMat.SetTexture("_MainTex", renderTexture);
+	bloomMat.SetTexture("_BlurTex", renderTexture);
+	bloomMat.SetFloat("_Threshold", 0);
+	bloomMat.SetFloat("_Amount", amount);
+}
+
+function OnPostRender () {
+	FullScreenQuad(bloomMat);
+}
---
+++ Lethal-Pursuit/Assets/Plugins/ImageEffects/ShaderDir/MotionBlurAlphaBlended.shader
@@ -0,0 +1,20 @@
+﻿Shader "IndieEffects/Alpha-Blended Motion Blur" {
+	Properties {
+		_MainTex ("Texture(RGB)", 2D) = "black" {}
+		_Sample2 ("Texture2(RGB)", 2D) = "black" {}
+		_FrameAccumulation ("Accumulation", float) = 0.65
+	}
+	
+	SubShader {
+		ZTest Always Cull Off ZWrite Off
+		Pass {
+			Blend SrcAlpha OneMinusSrcAlpha
+			ColorMask RGB
+			SetTexture [_MainTex] {
+				ConstantColor(1,1,1,[_FrameAccumulation])
+				Combine texture, constant
+			}
+		}
+	}
+	Fallback off
+}
