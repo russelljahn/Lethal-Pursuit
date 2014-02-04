0cd83e7440034adcc2c7d978423804d87d42163a
j <b>
2014-02-03 23:15:30 -0600

    Added health and collision damage, terrain now tagged "Unpassable", added audio files and script, added smoke

---
+++ Lethal-Pursuit/Assets/Audio2.js
@@ -0,0 +1,32 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+var engine4: AudioClip; 
+var engine3: AudioClip;
+var engine1: AudioClip;
+
+var audioClipSpeed = 10.0; //default audio clip speed
+
+function Update() {
+	var p = rigidbody.velocity.magnitude / audioClipSpeed;
+	audio.pitch = Mathf.Clamp( p, 0.1, 4.0); // p is clamped to limiting values .1 and 4
+	
+	
+	if(Input.GetButtonDown("Fire1")){
+		audio.Play();
+
+	}
+	
+	
+	if (Input.GetButtonUp("Fire1")){
+		// you can use PlayOneShot to specify the sound...
+		audio.Stop();
+		// or you can define the sound in the Inspector
+		// and use Play like this:
+	}
+	
+}
+
--- Lethal-Pursuit/Assets/Scripts/Spaceship.cs
+++ Lethal-Pursuit/Assets/Scripts/Spaceship.cs
@@ -4,103 +4,123 @@ using System.Collections;
 
 [RequireComponent (typeof (Rigidbody))]
 public class Spaceship : MonoBehaviour {
-
+	
 	public Vector3 instantaneousVelocity = new Vector3(0.0f, 0.0f, 2500.0f);
 	public Vector3 acceleration = new Vector3(160.0f, 160.0f, 160.0f);
 	public Vector3 deacceleration = new Vector3(5000.0f, 5000.0f, 15000000);
 	public Vector3 maxVelocity  = new Vector3(0.0f, 0.0f, 250.0f);
-
+	
 	public float xTiltSpeed = 1.5f;
 	public float yTiltSpeed = 3.3f;
-
-
+	
+	
 	public ParticleSystem leftBoosterFlames;
 	public ParticleSystem rightBoosterFlames;
+	public ParticleSystem leftBoosterSmoke;
+	public ParticleSystem rightBoosterSmoke;
 	public float boostParticleEmissionRate = 254f;
 	public float brakeParticleEmissionRate = 50f;
-
+	public float boostParticleEmissionSize = 2.5f;
+	public float brakeParticleEmissionSize = 1.5f;
+	public float boostSmokeEmissionRate = 12.32f;
+	public float brakeSmokeEmissionRate = 4f;
+	
+	
+	
+	
 	
 	public GameObject spaceshipModel;
-
-
+	
+	
 	public Vector3 rightTiltRotation = new Vector3(  0.0f,  0.0f,  -85.0f);
 	public Vector3 leftTiltRotation  = new Vector3(  0.0f,  0.0f,   85.0f);
-
+	
 	public Vector3 upTiltRotation    = new Vector3(-40.0f,  2.0f,   -3.0f);
 	public Vector3 downTiltRotation  = new Vector3( 4.0f,   2.0f,    3.0f);
-
+	
 	public Vector3 downRightTiltRotation  = new Vector3(  30.0f,  5.0f,  -40.0f);
 	public Vector3 downLeftTiltRotation   = new Vector3(  30.0f, -5.0f,   40.0f);
-
+	
 	public Vector3 upRightTiltRotation    = new Vector3( -30.0f,  5.0f,  -80.0f);
 	public Vector3 upLeftTiltRotation     = new Vector3( -30.0f, -5.0f,   80.0f);
-
+	
 	private Vector3 lastFrameTargetRotationEuler = Vector3.zero;
-
-
+	
+	
 	public float normalTurningRate = 115.0f;
 	public float brakingTurningRate = 300.0f;
 	
-
+	
 	public float timeUntilCompleteStopAfterBoost = 1.0f;
 	private float timeSinceLastBoost = 0.0f;
-
+	
 	public float timeUntilMaxTurning = 2.6f;
 	private float timeSinceStartedTurning = 0.0f;
-
+	
 	public float timeUntilMaxBoostUpDown = 1.0f;
 	private float timeSinceStartedBoostingUpDown = 0.0f;
-
+	
+	public static float health = 100; /*health, damage, & status */
+	private float maxHealth = 100;
+	private float damageHalfHit = 50;
+	private float damageOneHitKO = 100;
+	private float damageBufferTime = 2;
+	private float stall = 0;
+	
+	
+	
 	private GameplayManager gameplayManager;
-
+	
 	/* Tilt of analogue stick every frame. */
 	private float xTilt;
 	private float yTilt;
 	private float boostAmount;
 	private float brakeAmount;
-
-
-
+	
+	
+	
 	// Use this for initialization
 	void Start () {
 		gameplayManager = GameplayManager.instance;
 	}
-
-
-
-
-
+	
+	
+	
+	
+	
 	void Update() {
 		HandleInput();
 		HandleParticles();
+		Destroy();
 	}
-
-
-
-
+	
+	
+	
+	
 	// This happens at a fixed timestep
 	void FixedUpdate () {
-
+		
 		HandleRotation();
 		HandleMovement();
 		HandleTilt();
-
-
-
+		
+		
+		
 	}
 	
-
-
-
-
+	
+	
+	
+	
 	void HandleInput() {
 		xTilt = InputManager.ActiveDevice.LeftStickX.Value;		
 		yTilt = InputManager.ActiveDevice.LeftStickY.Value;
 		boostAmount = InputManager.ActiveDevice.RightTrigger.Value;
 		brakeAmount = InputManager.ActiveDevice.LeftTrigger.Value;
-
+		
 		Debug.Log ("boostAmount: " + boostAmount);
 		Debug.Log ("brakeAmount: " + brakeAmount);
+	
 		
 		/* Map keyboard diagonal axis amount to joystick diagonal axis amount. */
 		if (Mathf.Abs(xTilt) > 0.5f && Mathf.Abs(yTilt) > 0.5f) {
@@ -108,64 +128,75 @@ public class Spaceship : MonoBehaviour {
 			yTilt *= 0.5f;
 		}
 	}
-
-
-
-
-
+	
+	
+	
+	
+	
 	void HandleParticles() {
-
+		
 		if (brakeAmount == 0 && boostAmount > 0) {
 			leftBoosterFlames.emissionRate =  boostParticleEmissionRate;
 			rightBoosterFlames.emissionRate = boostParticleEmissionRate;
+			leftBoosterFlames.startSize =  boostParticleEmissionSize;
+			rightBoosterFlames.startSize = boostParticleEmissionSize;
+			leftBoosterSmoke.emissionRate = boostSmokeEmissionRate;
+			rightBoosterSmoke.emissionRate = boostSmokeEmissionRate;
+			
+			
 		}
 		else {
 			leftBoosterFlames.emissionRate = brakeParticleEmissionRate;
 			rightBoosterFlames.emissionRate = brakeParticleEmissionRate;
+			leftBoosterFlames.startSize = brakeParticleEmissionSize;
+			rightBoosterFlames.startSize = brakeParticleEmissionSize;
+			leftBoosterSmoke.emissionRate = brakeSmokeEmissionRate;
+			rightBoosterSmoke.emissionRate = brakeSmokeEmissionRate;
+			
 		}
-
+		
 	}
-
-
-
-
-
+	
+	
+	
+	
+	
 	void HandleMovement() {
-
-
+		
+		
 		/* Constrain max velocity. */
 		rigidbody.velocity = new Vector3(
 			Mathf.Min(rigidbody.velocity.x, maxVelocity.x),
 			Mathf.Min(rigidbody.velocity.y, maxVelocity.y),
 			Mathf.Min(rigidbody.velocity.z, maxVelocity.z)
-		);
-
+			);
+		
 		Vector3 forwardVector = spaceshipModel.transform.forward;
 		forwardVector.y = 0.0f;
-
+		
 		if (yTilt != 0) {
 			timeSinceStartedBoostingUpDown += Time.deltaTime;
 		}
 		else {
 			timeSinceStartedBoostingUpDown = timeUntilMaxBoostUpDown;
 		}
-
-
+		
+		
 		/* Forward boost. */
 		if (boostAmount > 0.0f && brakeAmount == 0.0f) {
-
+			
 			/* Boost forward. */
 			rigidbody.MovePosition(rigidbody.position + boostAmount*forwardVector*Time.deltaTime*acceleration.z);
-
+			
 			/* Move up/down. The amount to move increases at an increasing rate so that you feel like you're pushing 
 			 * into an up/down boost. */
 			rigidbody.MovePosition(
 				Vector3.Slerp(
-					rigidbody.position,
-					rigidbody.position + Vector3.up*yTilt*Time.deltaTime*acceleration.y,
-					Mathf.Clamp01(timeSinceStartedBoostingUpDown/timeUntilMaxBoostUpDown)
+				rigidbody.position,
+				rigidbody.position + Vector3.up*yTilt*Time.deltaTime*acceleration.y,
+				Mathf.Clamp01(timeSinceStartedBoostingUpDown/timeUntilMaxBoostUpDown)
 				)
-			);
+				);
 			
 			timeSinceLastBoost = 0.0f;
 		}
@@ -173,33 +204,33 @@ public class Spaceship : MonoBehaviour {
 			/* Boost forward at a decreasing rate (Eventually slow down). */
 			rigidbody.MovePosition(
 				Vector3.Slerp(
-					rigidbody.position + forwardVector*Time.deltaTime*acceleration.z,
-					rigidbody.position,
-					Mathf.Clamp01(timeSinceLastBoost/timeUntilCompleteStopAfterBoost)
+				rigidbody.position + forwardVector*Time.deltaTime*acceleration.z,
+				rigidbody.position,
+				Mathf.Clamp01(timeSinceLastBoost/timeUntilCompleteStopAfterBoost)
 				)
-			);
+				);
 			/* Boost up/down at a decreasing rate (Eventually slow down). */
 			rigidbody.MovePosition(
 				Vector3.Slerp(
-					rigidbody.position + Vector3.up*yTilt*Time.deltaTime*acceleration.y,
-					rigidbody.position,
-					Mathf.Clamp01(timeSinceLastBoost/timeUntilCompleteStopAfterBoost)
+				rigidbody.position + Vector3.up*yTilt*Time.deltaTime*acceleration.y,
+				rigidbody.position,
+				Mathf.Clamp01(timeSinceLastBoost/timeUntilCompleteStopAfterBoost)
 				)
-			);
-
+				);
+			
 			timeSinceLastBoost += Time.deltaTime;
 		}
-
+		
 	}
-
-
-
-
-
+	
+	
+	
+	
+	
 	void HandleTilt() {
-
+		
 		Vector3 targetRotationEuler = Vector3.zero;
-
+		
 		/* Based on analogue stick direction, figure out rotation state to blend to. */
 		if (xTilt == 0) {
 			if (yTilt == 0) {
@@ -234,69 +265,93 @@ public class Spaceship : MonoBehaviour {
 				targetRotationEuler = upRightTiltRotation;
 			}
 		}
-	
+		
 		/* Blend from current rotation towards target rotation. */
 		if (yTilt != 0) {
 			spaceshipModel.transform.localRotation = Quaternion.Slerp(
 				spaceshipModel.transform.localRotation, 
 				Quaternion.Euler((1.0f-.5f*(yTilt+1.0f))*lastFrameTargetRotationEuler + .5f*(yTilt+1.0f)*targetRotationEuler), 
 				yTiltSpeed*Time.deltaTime
-			);
+				);
 		}
 		if (xTilt != 0) {
 			spaceshipModel.transform.localRotation = Quaternion.Slerp(
 				spaceshipModel.transform.localRotation, 
 				Quaternion.Euler((1.0f-.5f*(xTilt+1.0f))*lastFrameTargetRotationEuler + .5f*(xTilt+1.0f)*targetRotationEuler), 
 				xTiltSpeed*Time.deltaTime
-			);
+				);
 		}
 		if (xTilt == 0 && yTilt == 0) {
 			spaceshipModel.transform.localRotation = Quaternion.Slerp(
 				spaceshipModel.transform.localRotation, 
 				Quaternion.Euler(targetRotationEuler), 
 				xTiltSpeed*Time.deltaTime
-			);
+				);
 		}
 		lastFrameTargetRotationEuler = targetRotationEuler;
 	}
-
-
-
-
-
-
+	
+	
+	
+	
+	
+	
 	void HandleRotation() {
-
-
+		
+		
 		if (xTilt != 0) {
-
+			
 			float turningRateForThisFrame = normalTurningRate;
-
+			
 			/* Allow drift turning if player is holding down brake. */
 			if (brakeAmount > 0.0f) {
 				turningRateForThisFrame = brakeAmount*brakingTurningRate;
 			}
-
-//			Debug.Log("turningRate: " + turningRateForThisFrame);
-
+			
+			//			Debug.Log("turningRate: " + turningRateForThisFrame);
+			
 			this.rigidbody.MoveRotation(
 				Quaternion.Slerp (
-					this.transform.localRotation,
-					Quaternion.Euler(this.transform.localRotation.eulerAngles + Vector3.up*xTilt*turningRateForThisFrame*Time.deltaTime),
-					Mathf.Clamp01(timeSinceStartedTurning/timeUntilMaxTurning)
+				this.transform.localRotation,
+				Quaternion.Euler(this.transform.localRotation.eulerAngles + Vector3.up*xTilt*turningRateForThisFrame*Time.deltaTime),
+				Mathf.Clamp01(timeSinceStartedTurning/timeUntilMaxTurning)
 				)
-			);
+				);
 			timeSinceStartedTurning += Time.deltaTime;
 		}
 		else {
 			timeSinceStartedTurning = 0.0f;
 		}
-
-	}
-
-
-
-
-
 		
+	}
+	
+	
+	void OnCollisionEnter(Collision walls) { /* hitting walls tagged as unpassable does 50 damage */
+		
+		if (walls.gameObject.tag == "Unpassable" && stall == 0) {
+			
+			DecreaseHealth ();
+
+		}
+	}
+	
+	void DecreaseHealth (){
+		
+		health -= damageHalfHit;
+		stall = 1;
+		/* yield WaitForSeconds (damageBufferTime);  damage buffer code, not working atm*/
+		stall = 0;
+		Debug.Log ("Health: " + health); 
+	}
+	
+	
+	void Destroy() {
+		
+		/* if (health <= 0){
+				Destroy(gameObject);*/
+		
+	}
+	
+	
+	
 }
