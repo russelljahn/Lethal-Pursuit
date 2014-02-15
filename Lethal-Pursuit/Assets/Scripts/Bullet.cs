950617da08474b65fd34b2761321029bfa5b1c5d
russelljahn <russelljahn@gmail.com>
2014-02-15 13:26:36 -0600

    Added basic gun/shooting to spaceship002 prefab, bullets don't explode upon contact yet.

--- Lethal-Pursuit/Assets/Scripts/Bullet.cs
+++ Lethal-Pursuit/Assets/Scripts/Bullet.cs
@@ -6,12 +6,14 @@ public class Bullet : MonoBehaviour {
 	public Vector3 direction;
 	public float speed = 50f;
 	public float timeUntilDeath = 5.0f;
-	public bool prefab = true;
+//	public bool prefab = true;
+	public bool alreadyActivatedOnDeath = false;
 
+	public Detonator detonater;
 
 	// Use this for initialization
 	void Start () {
-		Debug.Log ("Bullet.transform.position: " + this.transform.position);
+//		Debug.Log ("Bullet.transform.position: " + this.transform.position);
 		direction.Normalize();
 	}
 
@@ -22,17 +24,38 @@ public class Bullet : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if (!prefab) {
+//		if (!prefab) {
 			timeUntilDeath -= Time.deltaTime;
-			if (timeUntilDeath <= 0) {
-				GameObject.Destroy(this.gameObject);
+			if (timeUntilDeath <= 0 && !alreadyActivatedOnDeath) {
+				alreadyActivatedOnDeath = true;
+				StartCoroutine(OnDeath());
 			}
 
 			this.rigidbody.MovePosition(this.transform.position + speed*direction*Time.deltaTime);
-		}
+//		}
 	}
 
 
+	IEnumerator OnDeath () {
+		GameObject.Destroy(this.gameObject);
+		yield break;
+	}
 
+
+	void OnCollisionEnter(Collision collision) {
+		if (!alreadyActivatedOnDeath && ShouldExplodeOnContact(collision.gameObject)) {
+			Debug.Log (this.gameObject.name + "collided with: " + collision.gameObject.name);
+			
+			alreadyActivatedOnDeath = true;
+			detonater.Explode();
+			StartCoroutine(OnDeath());
+		}
+
+	}
+
+
+	bool ShouldExplodeOnContact(GameObject gameObject) {
+		return !(gameObject.CompareTag("Bullet") || gameObject.CompareTag("Spaceship"));
+	}
 
 }
--- Lethal-Pursuit/Assets/Scripts/Spaceship/SpaceshipGun.cs
+++ Lethal-Pursuit/Assets/Scripts/Spaceship/SpaceshipGun.cs
@@ -4,10 +4,11 @@ using System.Collections;
 
 public class SpaceshipGun : SpaceshipComponent {
 
-	public Bullet bulletToUse;
+	public string bulletResourcePath = "Bullets/TestLaz0r";
+//	public Bullet bulletToUse;
 
-	public float bufferTimeBetweenShots = 0.1f;
-	public float timeUntilCanShoot = 0.0f;
+	public float cooldownBetweenShots = 0.1f;
+	private float timeUntilCanShoot = 0.0f;
 
 	public float forceFactor = 1.0f;
 
@@ -36,14 +37,24 @@ public class SpaceshipGun : SpaceshipComponent {
 	void FixedUpdate() {
 
 		if (shooting && timeUntilCanShoot == 0.0f) {
-			Debug.Log ("SpaceshipGun.transform.position: " + this.transform.position);
-			Bullet bullet = GameObject.Instantiate(bulletToUse, this.transform.position, Quaternion.identity) as Bullet;
+//			Debug.Log ("SpaceshipGun.transform.position: " + this.transform.position);
+
+			GameObject bulletGameObject = GameObject.Instantiate(
+				Resources.Load(bulletResourcePath, typeof(GameObject)), 
+				this.transform.position, 
+				Quaternion.identity
+			) as GameObject;
+
+			Bullet bullet = bulletGameObject.GetComponent<Bullet>();
 			bullet.direction = spaceshipModel.transform.forward;
-			bullet.prefab = false;
+			bullet.speed = Mathf.Max(bullet.speed, 1.15f*spaceship.currentVelocity);
+//			bullet.prefab = false;
+
+			timeUntilCanShoot = cooldownBetweenShots;
 		}
-
-		timeUntilCanShoot = Mathf.Max(0.0f, timeUntilCanShoot - Time.deltaTime);
-
+		else {
+			timeUntilCanShoot = Mathf.Max(0.0f, timeUntilCanShoot - Time.deltaTime);
+		}
 	}
 
 
