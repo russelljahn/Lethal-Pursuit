84249d2f877346713265bac9ff673a19090344eb
russelljahn <russelljahn@gmail.com>
2014-02-24 17:47:37 -0600

    Added functioning healthbar to GUI.

---
+++ Lethal-Pursuit/Assets/Scripts/GUI/HUD_Health.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class HUD_Health : MonoBehaviour {
+
+	public Color healthyColor = Color.green;
+	public Color injuredColor = Color.yellow;
+	public Color criticalColor = Color.red;
+
+	private SpaceshipStatus status;
+	private UI2DSprite healthbarSprite;
+
+	private Vector3 initialScale;
+	private Vector3 currentScale;
+
+
+	// Use this for initialization
+	void Start () {
+		status = GameObject.FindWithTag("Spaceship").GetComponent<SpaceshipStatus>();
+		healthbarSprite = GetComponent<UI2DSprite>();
+		initialScale = this.transform.localScale;
+		currentScale = initialScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentScale.x = status.currentHealth/status.maxHealth * initialScale.x;
+		this.transform.localScale = currentScale;
+
+		switch (status.healthState) {
+			case HealthState.HEALTHY:
+				healthbarSprite.color = healthyColor;
+				break;
+			case HealthState.INJURED:
+				healthbarSprite.color = injuredColor;
+				break;
+			case HealthState.CRITICAL:
+				healthbarSprite.color = criticalColor;
+				break;
+			default:
+				throw new Exception("Unknown health state: " + status.healthState);
+		}
+	}
+}
--- Lethal-Pursuit/Assets/Scripts/Spaceship/SpaceshipStatus.cs
+++ Lethal-Pursuit/Assets/Scripts/Spaceship/SpaceshipStatus.cs
@@ -2,11 +2,23 @@
 using System.Collections;
 using InControl;
 
+public enum HealthState {
+	HEALTHY,
+	INJURED,
+	CRITICAL
+}
+
 public class SpaceshipStatus : SpaceshipComponent {
 
+		public float currentHealth = 100;
+		public float maxHealth = 100;
+	
+		public HealthState healthState = HealthState.HEALTHY;
 
-		public static float health = 100; /*health, damage, & status */
-		private float maxHealth = 100;
+		public float healthRatioToBeInjured = 0.60f;
+		public float healthRatioToBeCritical = 0.30f;
+	
+
 		private float damageHalfHit = 50;
 		private float damageOneHitKO = 100;
 		private float damageBufferTime = 2;
@@ -21,67 +33,83 @@ public class SpaceshipStatus : SpaceshipComponent {
 
 	void Start() {
 		
-		health = 100;
-		label = GetComponent<UILabel>();
-	}
-		
-	void OnCollisionEnter(Collision interactibles) { /* hitting walls tagged as unpassable does 50 damage */
-
-		if (interactibles.gameObject.tag == "Unpassable" ) {
-
-			DecreaseHealth ();
-			detonator1.Explode();
-			detonator2.Explode();
-			ShakeCamera(1, 1);
-			fadeIn();
-			}
-		if (interactibles.gameObject.tag == "Health Pack") { //+50 health
-				
-				health = Mathf.Min (health+50, maxHealth);
-			}
-							
+		currentHealth = maxHealth;
+//		label = GetComponent<UILabel>();
 	}
 
-			
-	void DecreaseHealth (){
-			
-				if (health > 0) {
-						health -= damageHalfHit;
-						stall = 1;
-						/* yield WaitForSeconds (damageBufferTime);  damage buffer code, not working atm, use invoke?*/
-						stall = 0;
-						
-						}
-				else if (health == 0) {
-							
-								//Debug.Log ("you died, scrub");
-						}
-					}
-		
-
-	public void ShakeCamera(float magnitude, float time) {
-
-			Vector3 magnitudeVector = new Vector3 (magnitude, magnitude, 0.0f);
-			iTween.ShakePosition (GetComponent<SpaceshipCamera>().gameObject, magnitudeVector, time);
-
-		}
-
-	public void fadeIn () {
-		iTween.CameraFadeAdd(damageOverlay);
-		iTween.CameraFadeFrom(0,.5f);
-		iTween.CameraFadeTo(1.0f, 1.0f);
-
-
-
-	}
 
 	void Update() {
+		float fractionOfMaxHealth = currentHealth/maxHealth;
 
-
-		label.text = health.ToString(); 
-
+		if (fractionOfMaxHealth <= healthRatioToBeCritical) {
+			healthState = HealthState.CRITICAL;
+		}
+		else if (fractionOfMaxHealth <= healthRatioToBeInjured) {
+			healthState = HealthState.INJURED;
+		}
+		else {
+			healthState = HealthState.HEALTHY;
+		}
 
 	}
+		
+//	void OnCollisionEnter(Collision interactibles) { /* hitting walls tagged as unpassable does 50 damage */
+//
+//		if (interactibles.gameObject.tag == "Unpassable" ) {
+//
+//			DecreaseHealth ();
+//			detonator1.Explode();
+//			detonator2.Explode();
+//			ShakeCamera(1, 1);
+//			fadeIn();
+//			}
+//		if (interactibles.gameObject.tag == "Health Pack") { //+50 health
+//				
+//				health = Mathf.Min (health+50, maxHealth);
+//			}
+//							
+//	}
+//
+//			
+//	void DecreaseHealth (){
+//			
+//				if (health > 0) {
+//						health -= damageHalfHit;
+//						stall = 1;
+//						/* yield WaitForSeconds (damageBufferTime);  damage buffer code, not working atm, use invoke?*/
+//						stall = 0;
+//						
+//						}
+//				else if (health == 0) {
+//							
+//								//Debug.Log ("you died, scrub");
+//						}
+//					}
+//		
+//
+//	public void ShakeCamera(float magnitude, float time) {
+//
+//			Vector3 magnitudeVector = new Vector3 (magnitude, magnitude, 0.0f);
+//			iTween.ShakePosition (GetComponent<SpaceshipCamera>().gameObject, magnitudeVector, time);
+//
+//		}
+//
+//	public void fadeIn () {
+//		iTween.CameraFadeAdd(damageOverlay);
+//		iTween.CameraFadeFrom(0,.5f);
+//		iTween.CameraFadeTo(1.0f, 1.0f);
+//
+//
+//
+//	}
+//
+//	void Update() {
+//
+//
+//		label.text = health.ToString(); 
+//
+//
+//	}
 
 
 
