c7ae820bcedbb0253dc8cb571509b751c5452457
russelljahn <russelljahn@gmail.com>
2014-02-24 23:20:54 -0600

    Switched ship gun bulllets to laz0rs.

--- Lethal-Pursuit/Assets/Scripts/Spaceship/SpaceshipGun.cs
+++ Lethal-Pursuit/Assets/Scripts/Spaceship/SpaceshipGun.cs
@@ -4,25 +4,38 @@ using System.Collections;
 
 public class SpaceshipGun : SpaceshipComponent {
 
-	public string bulletResourcePath = "Bullets/TestLaz0r";
+//	public string laserResourcePath = "Bullets/TestLaz0r";
 
-	public float cooldownBetweenShots = 0.1f;
-	private float timeUntilCanShoot = 0.0f;
-
-	public float forceFactor = 1.0f;
-
-	private GameObject cachedBullet;
+//	public float cooldownBetweenShots = 0.1f;
+//	private float timeUntilCanShoot = 0.0f;
+	
 	public AudioSource guns;
 	public AudioClip shot;
 
+	private Light light;
+	private LineRenderer line;
 
+	private float initialWidth = 0.075f;
+	public float laserWaverRate = 4.0f;
+
+	public float laserMaterialScrollSpeedU = 1.0f;
+	public float laserMaterialScrollSpeedV = 1.0f;
+
+	public float laserHitForce = 1000.0f;
+	
 
 	// Use this for initialization
 	public override void Start () {
 		base.Start();
 
-		cachedBullet = Resources.Load(bulletResourcePath, typeof(GameObject)) as GameObject;
-		cachedBullet.SetActive(false);
+		line = GetComponent<LineRenderer>();
+		light = GetComponent<Light>();
+
+		line.enabled = false;
+		light.enabled = false;
+
+//		cachedBullet = Resources.Load(bulletResourcePath, typeof(GameObject)) as GameObject;
+//		cachedBullet.SetActive(false);
 	}
 
 
@@ -32,6 +45,30 @@ public class SpaceshipGun : SpaceshipComponent {
 	// Update is called once per frame
 	public override void Update () {
 		base.Update();	
+
+		if (shooting) {
+			line.enabled = true;
+			light.enabled = true;
+
+			Ray ray = new Ray(this.transform.position, forward);
+			RaycastHit hit;
+
+			if (Physics.Raycast(ray, out hit)) {
+				line.SetPosition(0, ray.origin);
+				line.SetPosition(1, hit.point);
+
+				if (hit.rigidbody != null) {
+					hit.rigidbody.AddForceAtPosition(ray.direction*laserHitForce, hit.point);
+				}
+			}
+		}
+		else {
+			line.enabled = false;
+			light.enabled = false;
+		}
+
+		line.SetWidth(initialWidth*Mathf.Cos(laserWaverRate*Time.time), initialWidth*Mathf.Sin(laserWaverRate*Time.time));
+		renderer.material.SetTextureOffset("_MainTex", new Vector2(Time.time*laserMaterialScrollSpeedU, Time.time*laserMaterialScrollSpeedV));
 	}
 
 
@@ -40,29 +77,6 @@ public class SpaceshipGun : SpaceshipComponent {
 
 	void FixedUpdate() {
 
-		if (shooting && timeUntilCanShoot == 0.0f) {
-
-			GameObject bulletGameObject = GameObject.Instantiate(
-				cachedBullet,
-				this.transform.position, 
-				Quaternion.identity
-			) as GameObject;
-
-			Bullet bullet = bulletGameObject.GetComponent<Bullet>();
-			bullet.direction = spaceshipModel.transform.forward;
-			bullet.sourceSpaceship = spaceship;
-			bullet.speed *= forceFactor;
-
-
-
-			bulletGameObject.SetActive(true);
-
-			timeUntilCanShoot = cooldownBetweenShots;
-			audio.PlayOneShot(shot);
-		}
-		else {
-			timeUntilCanShoot = Mathf.Max(0.0f, timeUntilCanShoot - Time.deltaTime);
-		}
 	}
 
 
