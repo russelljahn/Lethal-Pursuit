6dad095d39a003f33f672e3d5a6f94a9d6dac9ba
Russell Jahn <russelljahn@gmail.com>
2014-03-29 12:22:28 -0500

    Added initial code and particle effects for Aurora Cannon pickup.

---
+++ Lethal-Pursuit/Assets/Scripts/Pickups/PickupSpawnpoint.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(Collider))]
+public class PickupSpawnPoint : MonoBehaviour {
+
+
+	Pickup pickup;
+	public float respawnTime = 10.0f;
+	private float timeRemainingUntilRespawn = 0.0f;
+	public AudioClip pickupSound;
+	public GameObject visuals;
+
+
+	void Start() {
+		this.collider.isTrigger = true;
+	}
+
+
+	void Update() {
+		timeRemainingUntilRespawn = Mathf.Max (0.0f, timeRemainingUntilRespawn-Time.deltaTime);
+		if (!gameObject.activeInHierarchy && timeRemainingUntilRespawn == 0.0f) {
+			collider.enabled = true;
+			visuals.SetActive(true);
+		}
+	}
+
+
+	void OnTriggerEnter(Collider collider) {
+		
+		if (collider.gameObject.CompareTag ("Spaceship") && collider.gameObject.GetComponent<SpaceshipPickups>().CanPickup(pickup)) {
+			Debug.Log (this.gameObject.name + "was picked up by: " + collider.gameObject.name);
+			audio.PlayOneShot(pickupSound);
+
+			collider.gameObject.GetComponent<SpaceshipPickups>().GetPickup(GameObject.Instantiate(pickup.gameObject) as Pickup);
+			
+			if (NetworkManager.IsSinglePlayer()) {
+				collider.enabled = false;
+				visuals.SetActive(false);
+				timeRemainingUntilRespawn = respawnTime;
+			}
+			else {
+				networkView.RPC("NetworkHidePickupSpawnPoint", RPCMode.All);
+			}	
+		}
+		
+	}
+
+
+	[RPC]
+	void NetworkHidePickupSpawnPoint() {
+		collider.enabled = false;
+		visuals.SetActive(false);
+		timeRemainingUntilRespawn = respawnTime;
+	}
+
+	
+}
---
+++ Lethal-Pursuit/Assets/Scripts/Weapons/Pickup.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickup : MonoBehaviour {
+
+	Spaceship spaceship;
+
+	public virtual void Start() {
+
+	}
+
+	public virtual void Update() {
+
+	}
+
+	public virtual bool ShouldDrop() {
+		return false;
+	}
+
+	public virtual void OnPickup(Spaceship spaceship) {
+		this.spaceship = spaceship;
+	}
+
+	public virtual void OnDrop() {
+
+	}
+	
+}
