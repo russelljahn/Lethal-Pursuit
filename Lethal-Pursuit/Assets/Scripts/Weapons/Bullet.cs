12d68134b37ccd37e06c53c314ddb5c3a10a61ff
Russell Jahn <russelljahn@gmail.com>
2014-03-29 01:10:27 -0500

    Hex laser now does damage over multiplayer.

--- Lethal-Pursuit/Assets/Scripts/Weapons/Bullet.cs
+++ Lethal-Pursuit/Assets/Scripts/Weapons/Bullet.cs
@@ -6,7 +6,8 @@ public class Bullet : MonoBehaviour {
 	public Spaceship sourceSpaceship;
 	public GameObject explosion;
 
-	public Vector3 direction;
+	public Vector3 direction = Vector3.forward;
+	public float damage = 15f;
 	public float speed = 50f;
 	public float timeUntilDeath = 5.0f;
 	private bool alreadyDying = false;
@@ -24,10 +25,10 @@ public class Bullet : MonoBehaviour {
 
 	void OnCollisionEnter(Collision collision) {
 		if (!alreadyDying && ShouldExplodeOnContact(collision.gameObject)) {
-//			Debug.Log (this.gameObject.name + "collided with: " + collision.gameObject.name);
 			alreadyDying = true;
 			explosion.transform.parent = null;
 			explosion.SetActive(true);
+			HandleApplyingDamage(collision.gameObject);
 			GameObject.Destroy(this.gameObject);
 		}
 	}
@@ -35,6 +36,21 @@ public class Bullet : MonoBehaviour {
 
 	bool ShouldExplodeOnContact(GameObject other) {
 		return !(other.CompareTag("Bullet") || other == sourceSpaceship.gameObject);
+	}
+
+
+	void HandleApplyingDamage(GameObject hitGameObject) {
+		if (hitGameObject == null) {
+			return;
+		}
+		IDamageable damageableObject = (IDamageable)hitGameObject.GetComponent(typeof(IDamageable));
+		
+		//		Debug.Log ("hitGameObject: " + hitGameObject.name);
+		//		Debug.Log ("hitGameObject is IDamageable: " + (damageableObject is IDamageable));
+		
+		if (damageableObject != null) {
+			damageableObject.ApplyDamage(damage, hitGameObject, gameObject.name + " is calling ApplyDamage()!");
+		}
 	}
 
 }
