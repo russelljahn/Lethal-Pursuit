bfa9c6811180641bee48872395080c83cce07284
Russell Jahn <russelljahn@gmail.com>
2014-03-30 00:36:17 -0500

    Aurora cannon now fires when ship shoots and follows crosshairs direction.

--- Lethal-Pursuit/Assets/Scripts/Weapons/AuroraCannon.cs
+++ Lethal-Pursuit/Assets/Scripts/Weapons/AuroraCannon.cs
@@ -4,35 +4,29 @@ using System.Collections;
 [RequireComponent(typeof(LineRenderer))]
 public class AuroraCannon : Pickup {
 
-	public GameObject laserBeamEffect;
+	public PlaygroundParticles laserBeamEffect;
 	public float damageRate = 30.0f;
 	public float laserHitForce = 1000.0f;
 	public float currentEnergy = 100f;
 	public float energyDrainRate = 50.0f;
 	public float laserLength = 500.0f;
-	private LineRenderer line;
 	private GameObject hitGameObject;
 	
 
 	public override void Start() {
-		line = GetComponent<LineRenderer>();
 	}
 
 
 	public override void Update() {
-//		Debug.Log ("spaceship: " + spaceship);
 		if (spaceship != null && spaceship.shooting) {
-			line.enabled = true;
-			laserBeamEffect.SetActive(true);
 			currentEnergy -= energyDrainRate*Time.deltaTime;
+			laserBeamEffect.emissionRate = 1.0f;	
 			
 			Ray ray = new Ray(spaceship.spaceshipModel.transform.position, spaceship.spaceshipModel.transform.forward);
 			RaycastHit hit;
 
-			line.SetPosition(0, spaceship.spaceshipModel.transform.position);
 			if (Physics.Raycast(ray, out hit)) {
 				Debug.Log("ray hit: " + hit.collider.gameObject);
-				line.SetPosition(1, hit.point);
 				
 				/* Apply laser force. */
 				if (hit.rigidbody != null) {
@@ -41,13 +35,11 @@ public class AuroraCannon : Pickup {
 				}
 			}
 			else {
-				line.SetPosition(1, this.transform.InverseTransformPoint(new Vector3(0.0f, 0.0f, laserLength)));
 			}
 		}
 		else {
 			hitGameObject = null;
-			line.enabled = false;
-			laserBeamEffect.SetActive(false);
+			laserBeamEffect.emissionRate = 0.0f;
 		}
 	}
 
@@ -72,6 +64,9 @@ public class AuroraCannon : Pickup {
 	public override void OnPickup (Spaceship spaceship) {
 		base.OnPickup (spaceship);
 		spaceship.DisableGun();
+		this.transform.localPosition = Vector3.zero;
+		laserBeamEffect.transform.localScale = Vector3.one;
+		laserBeamEffect.sourceTransform = spaceship.spaceshipModel.transform;
 	}
 
 
